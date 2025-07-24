d212566853024dc07a9d3e5b28171b2ac268413b
NagaLakshmiVadde091999 <vaddenagalakshmi09@gmail.com>
2025-07-24 14:37:21 +0530

    JWT_Token Application

---
+++ src/main/java/com/example/JWT_TOKEN_AUTHORIZATION/JwtTokenAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWT_TOKEN_AUTHORIZATION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenAuthorizationApplication.class, args);
+	}
+
+}
